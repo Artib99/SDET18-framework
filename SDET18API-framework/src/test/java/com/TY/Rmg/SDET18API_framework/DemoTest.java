55bd7253380c53e8ec67d42858df536f5d0ced37
Artib99 <aarathib9999@gmail.com>
2021-08-04 13:56:03 +0530

    This is my first commit

---
+++ SDET18API-framework/src/test/java/com/TY/Rmg/SDET18API_framework/DemoTest.java
@@ -0,0 +1,15 @@
+package com.TY.Rmg.SDET18API_framework;
+
+import org.testng.annotations.Test;
+
+public class DemoTest {
+	@Test
+	public void demo()
+
+	
+	{
+		System.out.println("fiafhanjxn");
+		System.out.println("fiafhhygjganjxn");
+		
+	}
+}
